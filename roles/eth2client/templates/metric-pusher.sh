bcdf08b8a470ba2ab3dc092911573ab7e6a0ff9e
parithosh <parithosh.jayanthi@ethereum.org>
2021-03-24 16:05:05 +0530

    updated prysm with failover rpc and prater genesis config

--- roles/eth2client/templates/metric-pusher.sh
+++ roles/eth2client/templates/metric-pusher.sh
@@ -1,9 +1,20 @@
 function pull_to_push {
 
+  {%- if eth2client_name == "nimbus" %}
+
+  curl localhost:{{beacon_metrics_port}}/metrics | sed '/^[^#]/s/process_virtual_memory_bytes /_ /' | sed '/^[^#]/s/process_max_fds /_c /' | sed '/^[^#]/s/process_open_fds /_b /' | sed '/^[^#]/s/process_cpu_seconds_total /_d /' |sed '/^[^#]/s/process_start_time_seconds /_e /' | sed '/^[^#]/s/process_resident_memory_bytes /_f /' | curl --data-binary @- http://{{pushgateway_ip}}/metrics/job/pushgateway/scrape_location/beacon/network/{{eth2_network_name}}/instance/$(hostname)/client_name/{{eth2client_name}}
+
+  {% else %}
+
   curl localhost:{{beacon_metrics_port}}/metrics | curl --data-binary @- http://{{pushgateway_ip}}/metrics/job/pushgateway/scrape_location/beacon/network/{{eth2_network_name}}/instance/$(hostname)/client_name/{{eth2client_name}}
+
+  {% endif %}
+
+  {%- if raw_iron is sameas false %}
 
   curl localhost:9100/metrics | curl --data-binary @- http://{{pushgateway_ip}}/metrics/job/pushgateway/scrape_location/node_exporter/network/{{eth2_network_name}}/instance/$(hostname)/client/{{eth2client_name}}
 
+  {% endif %}
 
   {%- if validator_enabled is sameas true %}
 
